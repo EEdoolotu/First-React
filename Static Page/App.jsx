486a930dbfbd7a515f94ee708a1d44103272f1ba
EEdoolotu <ejirogheneedoolotu@gmail.com>
2025-10-18 19:38:04 +0100

    new

---
+++ Static Page/App.jsx
@@ -0,0 +1,14 @@
+
+import MainContent from "./Components/Main.jsx" 
+import Navbar from "./Components/Navbar.jsx"
+
+
+export default function App() {
+    return (
+        <div>
+            <Navbar />
+            <MainContent />
+        </div>
+        
+    )
+}
---
+++ Static Page/Components/Navbar.jsx
@@ -0,0 +1,8 @@
+export default function Navbar() {
+    return (
+        <header>
+            <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/2300px-React-icon.svg.png" alt="react-logo" width="50px" />
+            <h1>ReactFacts</h1>
+        </header>
+    )
+}
