d9337829eff0c47f09a2a5eee4ba36df67ea5a56
EEdoolotu <ejirogheneedoolotu@gmail.com>
2025-10-20 04:17:58 +0100

    morning

--- Kittens/App.jsx
+++ Kittens/App.jsx
@@ -4,26 +4,26 @@ export default function App() {
     return (
         <div className="contacts">
             <Contact 
-                img="./images/mr-whiskerson.png"
+                img="./images/ginger-cat.jpg"
                 name="Mr. Whiskerson"
                 phone="(212) 555-1234"
                 email="mr.whiskaz@catnap.meow"
             
             />
             <Contact 
-                img="./images/fluffykins.png"
+                img="./images/mikhail-vasilyev-IFxjDdqK_0U-unsplash.jpg"
                 name="Fluffykins"
                 phone="(212) 555-2345"
                 email="fluff@me.com"
             />
             <Contact 
-                img="./images/felix.png"
+                img="./images/omar-ramadan-q5U75AK69-A-unsplash.jpg"
                 name="Felix"
                 phone="(212) 555-4567"
                 email="thecat@hotmail.com"
             />
             <Contact 
-                img="./images/pumpkin.png"
+                img="./images/tabby-cat.jpg"
                 name="Pumpkin"
                 phone="(0800) CAT KING"
                 email="pumpkin@scrimba.com"
